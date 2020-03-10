4fc7ad3b01ca0a91316302bc85d887451113061b
Brian Pan <brianpanzifan@gmail.com>
2020-03-09 22:24:46 -0400

    first commit

---
+++ data-manipulation-sql-retail-supply.sql
@@ -0,0 +1,10 @@
+/* This is the completed project of processing the data from a movie rental store using MySQL by Brian.
+This project reveal the typical retail related business case. This sript contains many steps and they are
+stated before each section of codes.*/
+select * from film limit 100;
+select * from store;
+select * from inventory limit 100;
+select * from rental limit 100;
+select * from category limit 100;
+select * from payment limit 100;
+select * from film_category limit 100;
